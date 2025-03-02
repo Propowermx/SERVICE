44065a33dfd4036f1e11205a153a24208a9a36ea
Propowermx <info@propowermx.co.uk>
2025-03-02 11:07:46 +0000

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,22 @@
+const CACHE_NAME = "ppmx-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/manifest.json",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/service-worker.js",
+    "/static/js/bundle.js",
+    "/static/css/main.css"
+];
+
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open("ppmx-cache").then((cache) => {
-            return cache.addAll(["/"]);
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
         })
     );
+    self.skipWaiting(); // Activate worker immediately
 });
 
 self.addEventListener("fetch", (event) => {
@@ -13,3 +26,17 @@ self.addEventListener("fetch", (event) => {
         })
     );
 });
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+    self.clients.claim(); // Take control of open pages
+});
+
